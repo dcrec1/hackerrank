8a0b0c2b9b3bca07e16c88907cc5524f39aa96a9
Diego Carrion <dc.rec1@gmail.com>
2015-07-07 19:33:55 -0300

    setup access token when testing

--- spec/hackerrank_spec.rb
+++ spec/hackerrank_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe HackerRank do
+  before :all do
+    HackerRank.access_token = ENV['HACKERRANK_ACCESS_TOKEN'] || "TEST"
+  end
+
   it 'has a version number' do
     expect(HackerRank::VERSION).not_to be nil
   end
