eaa4d8b2a42ff2e36de649436c1869ee1ab2a39c
Diego Carrion <dc.rec1@gmail.com>
2015-07-07 19:32:37 -0300

    fix spec failing by wrong constant name

--- spec/hackerrank_spec.rb
+++ spec/hackerrank_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe HackerRank do
   it 'has a version number' do
-    expect(Hackerrank::VERSION).not_to be nil
+    expect(HackerRank::VERSION).not_to be nil
   end
 
   it 'does something useful' do
