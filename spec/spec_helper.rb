00a9c8208810f934cf521db39e21d60504f7bea0
Diego Carrion <dc.rec1@gmail.com>
2015-07-07 19:29:34 -0300

    configure vcr
    * use webmock as the mock library
    * filter access token as is private
    * avoid persisting the response as binary so we 
    can modify the ids and keys

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'hackerrank'
+require 'vcr'
+
+VCR.configure do |c|
+  c.hook_into :webmock
+  c.cassette_library_dir = 'spec/cassettes'
+  c.filter_sensitive_data('<HACKERRANK_ACCESS_TOKEN>') { HackerRank.access_token }
+  c.before_record do |i|
+    i.response.body.force_encoding 'UTF-8'
+  end
+end
