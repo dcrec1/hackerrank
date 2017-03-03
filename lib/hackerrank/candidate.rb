db89017585cfc9fe1731d4c2d36775e5d8185351
Dimitri Kurashvili <dimakura@gmail.com>
2017-03-03 14:11:49 +0400

    Fix HackerRank::Candidate request
    HackerRank::Candidate sent {test_id:1234} body with every request,
    which caused errors during API request. Solution for this problem is to
    send empty bodies.

--- lib/hackerrank/candidate.rb
+++ lib/hackerrank/candidate.rb
@@ -1,6 +1,6 @@
 module HackerRank
   class Candidate < Base
-    collection_path -> (params) { "/tests/#{params[:test_id]}/candidates" }
+    collection_path -> (params) { "/tests/#{params.delete(:test_id)}/candidates" }
 
     def self.find_by_username(username, params)
       request :get, "/view", params, { username: username }
